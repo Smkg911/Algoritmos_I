92b9b494573c5d1dc3f05b32b2b0ffd94f9f8b94
Smkg911 <117240173+Smkg911@users.noreply.github.com>
2024-02-24 19:39:40 -0500

    Update CDT.py

--- CDT.py
+++ CDT.py
@@ -7,3 +7,18 @@ class CDT:
     valorInvertido=0
     interesMensual=0
     mesApertura=0
+
+    '''---------------------------
+    # Metodos 
+    ------------------------------'''
+
+    def ValorInvertido(self):
+        #aqui va el codigo
+        return "su Valor Invertido actual es"+ self.valorInvertido
+        
+    def InteresMensual(self):
+        #aqui va el codigo
+        interes = self.valorInvertido*0.1
+        nValorInvertido = self.valorInvertido+interes
+        self.valorInvertido = nValorInvertido
+        return self.valorInvertido
