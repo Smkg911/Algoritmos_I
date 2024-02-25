b45a18b5e373880317525bed34791a4fd3493cb2
Smkg911 <117240173+Smkg911@users.noreply.github.com>
2024-02-24 19:22:44 -0500

    Update CuentaAhorros.py

--- CuentaAhorros.py
+++ CuentaAhorros.py
@@ -6,3 +6,30 @@ class CuentaAhorros:
     --------------------------'''
     saldo=0
     interesMensual=0
+
+    '''---------------------------
+    # Metodos 
+    ------------------------------'''
+
+    def ConsultarSaldo(self):
+        #aqui va el codigo
+        return "su salario actual es"+ self.saldo 
+        
+    def InteresMensual(self):
+        #aqui va el codigo
+        interes = self.saldo*0.006
+        nSaldo = self.saldo+interes
+        self.saldo = nSaldo
+        return self.saldo
+
+    def DepositarValor(self):
+        #aqui va el codigo
+        deposito = 0
+        nSaldo = self.saldo+deposito
+        self.saldo = nSaldo 
+        
+    def RetirarValor(self):
+        #aqui va el codigo
+        retiro = 0
+        nSaldo = self.saldo-retiro 
+        self.saldo = nSaldo 
