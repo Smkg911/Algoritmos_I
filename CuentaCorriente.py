d177adbb819b7d79541f0380b02d74f3963c9b3f
Smkg911 <117240173+Smkg911@users.noreply.github.com>
2024-02-24 19:40:52 -0500

    Update CuentaCorriente.py

--- CuentaCorriente.py
+++ CuentaCorriente.py
@@ -5,3 +5,11 @@ class CuentaCorriente:
     #Atributos 
     -----------------------------'''
     saldo=0
+
+    '''---------------------------
+    # Metodos 
+    ------------------------------'''
+
+    def ConsultarSaldo(self):
+        #aqui va el codigo
+        return "su salario actual es"+ self.saldo
