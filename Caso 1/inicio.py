552a686bf21219365b61b539457416bb40cc919d
Smkg911 <117240173+Smkg911@users.noreply.github.com>
2024-03-01 15:55:32 -0500

    Create inicio.py

---
+++ Caso 1/inicio.py
@@ -0,0 +1,7 @@
+from Empleados import Empleados
+
+empleado1=Empleados("Juan", "Perez", 1000000, 1)
+empleado2=Empleados("Pedro", "Martinez", 6000000, 1)
+empleado3=Empleados("Maria", "Benitez", 6000000, 2)
+
+print(empleado2.ImpuestoAnual())
